5732970794795ed29294ca384101c4378f20965c
Fachry Dwi Afriza <fachry.dwi.fd.fda@gmail.com>
2023-08-21 18:55:52 +0700

    Add: Color

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,14 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: {
+          bg: "#15181D",
+          yellow: "#FEDE24",
+        },
+      },
+    },
   },
   plugins: [],
 };
