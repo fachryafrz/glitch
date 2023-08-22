f735f6f09ac66f0a47e244332d450d417a3214cb
Fachry Dwi Afriza <fachry.dwi.fd.fda@gmail.com>
2023-08-22 14:30:11 +0700

    Update: navbar

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,4 +1,3 @@
-/* eslint-disable @typescript-eslint/no-explicit-any */
 import { Link } from "react-router-dom";
 
 export default function Card({ game }) {
--- src/sections/Navbar.jsx
+++ src/sections/Navbar.jsx
@@ -5,11 +5,14 @@ import * as Icons from "ionicons/icons";
 import { IonIcon } from "@ionic/react";
 
 export default function Navbar() {
-  const [icons, setIcons] = useState([]);
+  const [navIcons, setNavIcons] = useState([]);
+  const [socialIcons, setSocialIcons] = useState([]);
+  const [active, setActive] = useState(false);
 
   useEffect(() => {
     const getIcons = () => {
-      setIcons(data.links.map((link) => Icons[link.icon]));
+      setNavIcons(data.links.map((item) => Icons[item.icon]));
+      setSocialIcons(data.socials.map((item) => Icons[item.icon]));
     };
 
     getIcons();
@@ -17,9 +20,22 @@ export default function Navbar() {
 
   return (
     <nav
-      className={`container mx-auto grid grid-cols-[1fr_auto_1fr] p-4 lg:px-24 gap-4 bg-primary-bg bg-opacity-90 backdrop-blur sticky top-0 z-50`}
+      className={`md:container mx-auto grid grid-cols-[1fr_auto_1fr] p-4 xl:px-24 gap-4 bg-primary-bg bg-opacity-90 backdrop-blur sticky top-0 z-50`}
     >
-      <ul className={`flex items-center gap-12 place-self-start h-full`}>
+      <button
+        onClick={() => setActive(!active)}
+        className={`grid place-items-center max-w-fit aspect-square lg:hidden`}
+      >
+        <IonIcon
+          icon={active ? Icons.closeOutline : Icons.menuOutline}
+          className={`text-3xl`}
+        />
+      </button>
+      <ul
+        className={`${
+          active ? `flex` : `hidden`
+        } lg:!flex gap-12 place-self-start absolute top-full left-0 flex-col lg:flex-row bg-primary-bg h-fit lg:static p-4 ml-4 lg:ml-0 lg:p-0 lg:h-full lg:items-center lg:bg-transparent`}
+      >
         {data.links.map((item, i) => {
           return (
             <li key={i}>
@@ -27,7 +43,7 @@ export default function Navbar() {
                 to={item.url}
                 className={`opacity-50 hocus:opacity-100 flex items-center gap-1`}
               >
-                <IonIcon icon={icons[i]} />
+                <IonIcon icon={navIcons[i]} />
                 <span>{item.name}</span>
               </Link>
             </li>
@@ -37,6 +53,20 @@ export default function Navbar() {
       <Link to={"/"}>
         <img src={`/logo.svg`} alt={`GameCove`} className={`w-10`} />
       </Link>
+      <ul className={`flex items-center justify-end h-full gap-4`}>
+        {data.socials.map((item, i) => {
+          return (
+            <li key={i}>
+              <Link to={item.url} className={`flex`}>
+                <IonIcon
+                  icon={socialIcons[i]}
+                  className={`text-2xl opacity-50 hocus:opacity-100`}
+                />
+              </Link>
+            </li>
+          );
+        })}
+      </ul>
     </nav>
   );
 }
