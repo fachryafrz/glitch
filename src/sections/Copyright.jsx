28b91360da85d5608c723c260db77f2bb49831e2
Fachry Dwi Afriza <fachry.dwi.fd.fda@gmail.com>
2023-08-25 08:48:09 +0700

    Update: Simplify text

--- src/sections/Copyright.jsx
+++ src/sections/Copyright.jsx
@@ -1,20 +1,20 @@
 export default function Copyright() {
   return (
     <p className="text-center p-2 text-sm cursor-default">
-      Designed by{" "}
+      Design by{" "}
       <a
         href="https://www.behance.net/Reshetnikov"
         target="_blank"
-        className="text-[#ea4c89] hocus:font-medium transition-all"
+        className="text-[#ea4c89] font-medium"
         rel="noreferrer"
       >
         Dmitry Reshetnikov
       </a>{" "}
-      <br className={`sm:hidden`} />& Developed by{" "}
+      <br className={`sm:hidden`} />& Built by{" "}
       <a
         href="https://fachryafrz.vercel.app"
         target="_blank"
-        className="text-[#ea4c89] hocus:font-medium transition-all"
+        className="text-[#ea4c89] font-medium"
         rel="noreferrer"
       >
         Fachry Dwi Afriza
