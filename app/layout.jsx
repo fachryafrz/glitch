301c999d58a2daf1f5df2d3869cce9136694849e
Fachry Dwi Afriza <fachry.dwi.fd.fda@gmail.com>
2023-10-11 16:55:57 +0700

    Fix 11 Okt 2023: Metadata

--- app/layout.jsx
+++ app/layout.jsx
@@ -3,75 +3,73 @@ import Navbar from "./sections/Navbar";
 import Footer from "./sections/Footer";
 import Copyright from "./sections/Copyright";
 
-export const metadata = {
-  generator: "GameCove",
-  applicationName: "GameCove",
-  referrer: "origin-when-cross-origin",
-  keywords: ["Game"],
-  authors: [
-    { name: "Fachry Dwi Afriza", url: "https://fachryafrz.vercel.app" },
-  ],
-  colorScheme: "dark",
-  creator: "Fachry Dwi Afriza",
-  publisher: "Fachry Dwi Afriza",
-  viewport: {
-    width: "device-width",
-    initialScale: 1,
-  },
-  title: {
-    default: "GameCove",
-    template: "%s - GameCove",
-  },
-  description: `Your ultimate destination for all things gaming! Explore the latest information about a wide range of games, from popular titles to upcoming releases. Our modern design and user-friendly interface provide an enjoyable browsing experience as you discover details about your favorite games. Whether you're a casual or hardcore gamer, GameCove is here to satisfy your curiosity.`,
-  metadataBase: new URL(`https://gamecove.vercel.app`),
-  alternates: {
-    canonical: "/",
-    languages: "en-US",
-  },
-  openGraph: {
-    title: "GameCove",
-    description: `Your ultimate destination for all things gaming! Explore the latest information about a wide range of games, from popular titles to upcoming releases. Our modern design and user-friendly interface provide an enjoyable browsing experience as you discover details about your favorite games. Whether you're a casual or hardcore gamer, GameCove is here to satisfy your curiosity.`,
-    url: process.env.APP_URL,
-    siteName: "GameCove",
-    images: "/logo.png",
-    locale: "en_US",
-    type: "website",
-  },
-  themeColor: "#202735",
-  manifest: "/manifest.json",
-  twitter: {
-    card: "summary_large_image",
-    title: "GameCove",
-    description: `Your ultimate destination for all things gaming! Explore the latest information about a wide range of games, from popular titles to upcoming releases. Our modern design and user-friendly interface provide an enjoyable browsing experience as you discover details about your favorite games. Whether you're a casual or hardcore gamer, GameCove is here to satisfy your curiosity.`,
-    creator: "@fachryafrz",
-    images: "/logo.png",
-  },
-  verification: {
-    google: "google",
-    yandex: "yandex",
-    yahoo: "yahoo",
-    other: {
-      me: ["fachrydwiafriza@gmail.com", "https://fachryafrz.vercel.app"],
-    },
-  },
-  category: "entertainment",
-  robots: {
-    index: false,
-    follow: true,
-    nocache: true,
-    googleBot: {
-      index: true,
-      follow: false,
-      noimageindex: true,
-      "max-video-preview": -1,
-      "max-image-preview": "large",
-      "max-snippet": -1,
-    },
-  },
-};
-
 export async function generateMetadata() {
-  return metadata;
+  return {
+    generator: "GameCove",
+    applicationName: "GameCove",
+    referrer: "origin-when-cross-origin",
+    keywords: ["Game"],
+    authors: [
+      { name: "Fachry Dwi Afriza", url: "https://fachryafrz.vercel.app" },
+    ],
+    colorScheme: "dark",
+    creator: "Fachry Dwi Afriza",
+    publisher: "Fachry Dwi Afriza",
+    viewport: {
+      width: "device-width",
+      initialScale: 1,
+    },
+    title: {
+      default: "GameCove",
+      template: "%s - GameCove",
+    },
+    description: `Your ultimate destination for all things gaming! Explore the latest information about a wide range of games, from popular titles to upcoming releases. Our modern design and user-friendly interface provide an enjoyable browsing experience as you discover details about your favorite games. Whether you're a casual or hardcore gamer, GameCove is here to satisfy your curiosity.`,
+    metadataBase: new URL(`https://gamecove.vercel.app`),
+    alternates: {
+      canonical: "/",
+      languages: "en-US",
+    },
+    openGraph: {
+      title: "GameCove",
+      description: `Your ultimate destination for all things gaming! Explore the latest information about a wide range of games, from popular titles to upcoming releases. Our modern design and user-friendly interface provide an enjoyable browsing experience as you discover details about your favorite games. Whether you're a casual or hardcore gamer, GameCove is here to satisfy your curiosity.`,
+      url: process.env.APP_URL,
+      siteName: "GameCove",
+      images: "/logo.png",
+      locale: "en_US",
+      type: "website",
+    },
+    themeColor: "#202735",
+    manifest: "/manifest.json",
+    twitter: {
+      card: "summary_large_image",
+      title: "GameCove",
+      description: `Your ultimate destination for all things gaming! Explore the latest information about a wide range of games, from popular titles to upcoming releases. Our modern design and user-friendly interface provide an enjoyable browsing experience as you discover details about your favorite games. Whether you're a casual or hardcore gamer, GameCove is here to satisfy your curiosity.`,
+      creator: "@fachryafrz",
+      images: "/logo.png",
+    },
+    verification: {
+      google: "google",
+      yandex: "yandex",
+      yahoo: "yahoo",
+      other: {
+        me: ["fachrydwiafriza@gmail.com", "https://fachryafrz.vercel.app"],
+      },
+    },
+    category: "entertainment",
+    robots: {
+      index: false,
+      follow: true,
+      nocache: true,
+      googleBot: {
+        index: true,
+        follow: false,
+        noimageindex: true,
+        "max-video-preview": -1,
+        "max-image-preview": "large",
+        "max-snippet": -1,
+      },
+    },
+  };
 }
 
 export default function RootLayout({ children }) {
