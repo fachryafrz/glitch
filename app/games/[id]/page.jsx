c1c80f4285c21e3774196faddae285c2ae1b9fe2
Fachry Dwi Afriza <fachry.dwi.fd.fda@gmail.com>
2024-01-20 20:13:24 +0700

    Refactor generateMetadata function in page.jsx

--- app/games/[id]/page.jsx
+++ app/games/[id]/page.jsx
@@ -4,40 +4,46 @@ import GameMedia from "./components/GameMedia";
 import axios from "axios";
 import { fetchData } from "@/app/lib/fetchData";
 
-// export async function generateMetadata({ params }) {
-//   const { slug } = params;
-//   const game = await fetchGame(slug);
-//   const images = await fetchGame(slug, "/screenshots");
-//   const stores = await fetchGame(slug, "/stores");
+export async function generateMetadata({ params }) {
+  const { id } = params;
 
-//   const filmReleaseDate = game.released
-//     ? new Date(game.released).getFullYear()
-//     : `Coming soon`;
+  const gameData = await fetchData({
+    path: `/games`,
+    fields: `
+    f *, screenshots.*, artworks.*, cover.*, genres.*, involved_companies.*, platforms.*, videos.*, age_ratings.*, external_games.*;
+    w id = ${id};
+    `,
+  });
+  const game = gameData[0];
 
-//   return {
-//     title: `${game.name} (${filmReleaseDate})`,
-//     description: game.description_raw,
-//     alternates: {
-//       canonical: `/${`games`}/${game.slug}`,
-//     },
-//     openGraph: {
-//       title: `${game.name} (${filmReleaseDate}) - GameCove`,
-//       description: game.description_raw,
-//       url: `https://gamecove.vercel.app/${`games`}/${game.slug}`,
-//       siteName: `GameCove`,
-//       images: game.background_image,
-//       locale: "en_US",
-//       type: "website",
-//     },
-//     twitter: {
-//       card: "summary_large_image",
-//       title: `${game.name} (${filmReleaseDate}) - GameCove`,
-//       description: game.description_raw,
-//       creator: "@fachryafrz",
-//       images: game.background_image,
-//     },
-//   };
-// }
+  const gameReleaseDate = game.first_release_date
+    ? new Date(game.first_release_date * 1000).getFullYear()
+    : `Coming soon`;
+
+  return {
+    title: `${game.name} (${gameReleaseDate})`,
+    description: game.summary,
+    alternates: {
+      canonical: `/games/${game.id}`,
+    },
+    openGraph: {
+      title: `${game.name} (${gameReleaseDate}) - GameCove`,
+      description: game.summary,
+      url: `https://gamecove.vercel.app/games/${game.id}`,
+      siteName: `GameCove`,
+      images: `https://images.igdb.com/igdb/image/upload/t_cover_big/${game.cover.image_id}.jpg`,
+      locale: "en_US",
+      type: "website",
+    },
+    twitter: {
+      card: "summary_large_image",
+      title: `${game.name} (${gameReleaseDate}) - GameCove`,
+      description: game.summary,
+      creator: "@fachryafrz",
+      images: `https://images.igdb.com/igdb/image/upload/t_cover_big/${game.cover.image_id}.jpg`,
+    },
+  };
+}
 
 export default async function Details({ params }) {
   const { id } = params;
